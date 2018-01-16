e4a39568fb32019c25bd0a6057923a755a6784b5
Simon Legner <Simon.Legner@gmail.com>
2018-01-16 23:05:08 +0100

    Obtain version via Atom feed, parse via gopkgbuild

--- main.go
+++ main.go
@@ -1,46 +1,65 @@
 package main
 
 import (
-	"context"
 	"flag"
 	"fmt"
 	"os"
 	"regexp"
 
-	"github.com/google/go-github/github"
 	"github.com/mikkeloscar/aur"
-	"golang.org/x/oauth2"
+	"github.com/mikkeloscar/gopkgbuild"
+	"github.com/mmcdole/gofeed"
 )
 
-func newGitHubClient(accessToken string) *github.Client {
-	token := &oauth2.Token{AccessToken: accessToken}
-	source := oauth2.StaticTokenSource(token)
-	client := oauth2.NewClient(context.TODO(), source)
-	return github.NewClient(client)
+func githubVersion(url string) (*pkgbuild.CompleteVersion, error) {
+	match := regexp.MustCompile("github.com/([^/#.]+)/([^/#.]+)").FindSubmatch([]byte(url))
+	if match == nil {
+		match = regexp.MustCompile("([^/#.]+).github.io/([^/#.]+)").FindSubmatch([]byte(url))
+	}
+	if match == nil {
+		return nil, fmt.Errorf("No GitHub release found for %s", url)
+	}
+
+	owner, repo := string(match[1]), string(match[2])
+	feedURL := fmt.Sprintf("https://github.com/%s/%s/releases.atom", owner, repo)
+	feed, err := gofeed.NewParser().ParseURL(feedURL)
+	if err != nil || len(feed.Items) == 0 {
+		return nil, fmt.Errorf("No GitHub release found for %s: %v", url, err)
+	}
+
+	link := regexp.MustCompile("/releases/tag/v?(.*)").FindSubmatch([]byte(feed.Items[0].Link))
+	if link == nil {
+		return nil, fmt.Errorf("No GitHub release found for %s: %v", url, err)
+	}
+	version := string(link[1])
+	return pkgbuild.NewCompleteVersion(version)
 }
 
-func handlePackage(client *github.Client, pkg *aur.Pkg) error {
-	githubURL := regexp.MustCompile("github.com/([^/]+)/([^/]+)")
-	match := githubURL.FindSubmatch([]byte(pkg.URL))
-	if match == nil {
-		return nil
+func handlePackage(pkg *aur.Pkg) error {
+
+	pkgVersion, err := pkgbuild.NewCompleteVersion(pkg.Version)
+	if err != nil {
+		return err
 	}
-	owner, repo := string(match[1]), string(match[2])
-	release, _, err := client.Repositories.GetLatestRelease(context.TODO(), owner, repo)
-	if err != nil || release == nil {
-		return fmt.Errorf("No release found for %s, %s: %v", pkg.Name, pkg.URL, err)
+
+	gitVersion, err := githubVersion(pkg.URL)
+	if err != nil {
+		return err
 	}
-	fmt.Println(pkg.Name, pkg.URL, pkg.Version, *release.Name, release.CreatedAt)
+	// workaround for https://github.com/mikkeloscar/gopkgbuild/pull/8
+	version := fmt.Sprintf("%d:%s-%s", pkgVersion.Epoch, gitVersion.Version, pkgVersion.Pkgrel)
+
+	fmt.Println(pkg.Name, pkg.URL, pkgVersion, gitVersion, pkgVersion.Older(version))
 	return nil
 }
 
-func handlePackageForMaintainer(maintainer string, client *github.Client) {
+func handlePackageForMaintainer(maintainer string) {
 	packages, err := aur.SearchByMaintainer(maintainer)
 	if err != nil {
 		panic(err)
 	}
 	for _, pkg := range packages {
-		err := handlePackage(client, &pkg)
+		err := handlePackage(&pkg)
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
 		}
@@ -49,12 +68,11 @@ func handlePackageForMaintainer(maintainer string, client *github.Client) {
 
 func main() {
 	user := flag.String("user", "", "AUR username")
-	token := flag.String("token", "", "GitHub personal access token")
 	flag.Parse()
 	if *user == "" {
 		fmt.Fprintln(os.Stderr, "-user is required")
 		flag.Usage()
 		os.Exit(1)
 	}
-	handlePackageForMaintainer(*user, newGitHubClient(*token))
+	handlePackageForMaintainer(*user)
 }
