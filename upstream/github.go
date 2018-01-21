ae0d5c100e3cf299c8e482b3d23563b2bb8c7876
Simon Legner <Simon.Legner@gmail.com>
2018-01-21 19:50:27 +0100

    Parse Atom feed using encoding/xml

--- upstream/github.go
+++ upstream/github.go
@@ -1,12 +1,25 @@
 package upstream
 
 import (
+	"encoding/xml"
 	"fmt"
+	"net/http"
 	"regexp"
 
 	pkgbuild "github.com/mikkeloscar/gopkgbuild"
-	"github.com/mmcdole/gofeed"
 )
+
+type atomFeed struct {
+	Items []atomEntry `xml:"entry"`
+}
+
+type atomEntry struct {
+	Link atomLink `xml:"link"`
+}
+
+type atomLink struct {
+	Href string `xml:"href,attr"`
+}
 
 func githubVersion(url string, re *regexp.Regexp) (*pkgbuild.CompleteVersion, error) {
 	match := re.FindSubmatch([]byte(url))
@@ -15,13 +28,20 @@ func githubVersion(url string, re *regexp.Regexp) (*pkgbuild.CompleteVersion, er
 	}
 
 	owner, repo := string(match[1]), string(match[2])
-	feedURL := fmt.Sprintf("https://github.com/%s/%s/releases.atom", owner, repo)
-	feed, err := gofeed.NewParser().ParseURL(feedURL)
+	resp, err := http.Get(fmt.Sprintf("https://github.com/%s/%s/releases.atom", owner, repo))
+	if err != nil {
+		return nil, fmt.Errorf("No GitHub release found for %s: %v", url, err)
+	}
+	defer resp.Body.Close()
+
+	dec := xml.NewDecoder(resp.Body)
+	var feed atomFeed
+	err = dec.Decode(&feed)
 	if err != nil || len(feed.Items) == 0 {
 		return nil, fmt.Errorf("No GitHub release found for %s: %v", url, err)
 	}
 
-	link := regexp.MustCompile("/releases/tag/v?(.*)").FindSubmatch([]byte(feed.Items[0].Link))
+	link := regexp.MustCompile("/releases/tag/v?(.*)").FindSubmatch([]byte(feed.Items[0].Link.Href))
 	if link == nil {
 		return nil, fmt.Errorf("No GitHub release found for %s: %v", url, err)
 	}
